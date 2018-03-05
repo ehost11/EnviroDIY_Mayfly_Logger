bd80f528466c8c57c0d0accafcf6609eb06e20f2
ehost11 <ehost96@gmail.com>
2018-03-04 21:00:55 -0600

    Cleaner script, more accurate variable names
    Removed unnecessary comments and renamed some variables to be more accurate descriptions of the actual variable.

--- capstone/water_level_attempt/water_level_attempt.ino
+++ capstone/water_level_attempt/water_level_attempt.ino
@@ -3,7 +3,8 @@
  * water pressure transducer and a Adafruit barometric pressure transducer
  * for the CLFLWD continuous groundwater monitor project.
  * I (Evan Host) am adapting code for each of the sensors from test sketches
- * provided by the company for their sensors (Adafruit MPL115A2 and SparkFun MS5803).
+ * provided by the companies for their sensors (Adafruit MPL115A2 and SparkFun MS5803)
+ * and from EnviroDIY.
  * Here goes nothing!
   */
 
@@ -12,7 +13,7 @@
 #include <Adafruit_MPL115A2.h> // MPL115A2 barometric pressure sensor library
 #include <SparkFun_MS5803_I2C.h> // MS5803 pressure transducer (used for water pressure) with I2C library
 #include <SPI.h> // SPI library
-#include <SD.h> // SD card library
+#include <SD.h> // MicroSD card library
 
 // Define variables
 // ADDRESS_HIGH = 0X76
@@ -22,36 +23,30 @@ MS5803 sensor(ADDRESS_HIGH);
 
 Adafruit_MPL115A2 mpl115a2;
 
-//Digital pin 12 is the MicroSD slave select pin on the Mayfly.
+//Define the SD pin: digital pin 12 is the MicroSD slave select pin on the Mayfly.
 #define SD_SS_PIN 12
 
-// The data log file for storing data on the SD card.
-#define FILE_NAME "PressureData.txt"
+// Define the data log file for storing data on the SD card.
+#define FILE_NAME "WaterLevel.txt"
 
-//Data header  (these lines get written to the beginning of a file when it's created)
+// Data header  (these lines get written to the beginning of a file when it's created)
 #define LOGGERNAME "Water Level attempt"
-#define DATA_HEADER "Sample Number, Water Pressure, Barometric Pressure"
+#define DATA_HEADER "Sample Number, Water Level"
 
 int samplenum = 1; // sample number starts at 1
-int time_conversion_factor = 1000; // Use this to convert easily from miliseconds to seconds 
-                                   // (just multiply it by the desired interval in seconds).
-int delaytime = 5 * time_conversion_factor; // delay time defines the delay interval between data collections.
-                                            // Delay time currently set at 5 seconds.
-                                            // Delay time variable should be spelled exactly the same as
-                                            // The delay time variable in other scripts to be used.
                        
 // I am defining the variables in the SparkFun test sketch even though we may not use them.
 float H2O_temperature_c, H20_temperature_f; // Floats are precise to 6 or 7 digits.
-float BaroPressure_mbar; // Multiplied by 10 to convert from kilopascals to millibar.
+float BaroPressure_mbar; // Measured in kpa; will need to be converted to mbar (just multiply by 10).
 double H2O_pressure_abs, H2O_pressure_relative, H2O_altitude_delta, H2O_pressure_baseline; // Doubles are precise to 15 digits.
-double reference_altitude = 0; // reference_altitude is currently equal to 0, or sea level (measured in m)
+double reference_altitude = 0; // reference_altitude is currently equal to 0, or sea level (measured in m).
 
 /*
  * SparkFun Comment
  "Thanks to Mike Grusin for letting me borrow the functions below from 
  the BMP180 example code."
  functions from the BMP180 example code are the following two variable definitions 
- (sealevel and altitude) and the two return functions.
+ (sealevel and altitude) and the two return functions. Not sure if we will need these.
 */
 double sealevel(double P, double A)
 // Given a pressure P (mbar) taken at a specific altitude (meters),
@@ -68,21 +63,22 @@ double altitude(double P, double P0)
 {
   return(44330.0*(1-pow(P/P0,1/5.255)));
 }
-  
+int time_conversion_factor = 1000; // Use this to convert easily from miliseconds to seconds 
+                                   // (just multiply it by the desired interval in seconds).
+int delaytime = 5 * time_conversion_factor; // delay time defines the delay interval between data collections.
+                                            // Delay time currently set at 5 seconds.
+                                            // Delay time variable should be spelled exactly the same as
+                                            // The delay time variable in other scripts to be used.
+
 void setup() 
 {
   // Define baud number
-  Serial.begin(9600);// Serial baud number (is this the correct one?)
-                     // (what is the difference between baud numbers?)
-                     // (it seems like this is the one used by all the programs I've used so far.)
-
-  
+  Serial.begin(9600);  
   
   // Retrieve calibration constants for calculation math.
   sensor.reset();
-  sensor.begin(); // This shouldn't mess things up, I think these are separate "begin" syntaxes for
-                  // the two libraries.
-  mpl115a2.begin();
+  sensor.begin(); 
+  mpl115a2.begin(); // This shouldn't mess things up, I think these are separate "begin" syntaxes for the two libraries.
 
   
   H2O_pressure_baseline = sensor.getPressure(ADC_4096); // This pressure_baseline variable might only be
@@ -93,11 +89,9 @@ void setup()
   setupLogFile();
   //print a line for data headers before starting to report data.
   Serial.println(DATA_HEADER);
-  
 }
 
 void loop() {
-
 
   // I am omitting the temperature calculations and sensing because we will have a separate sensor for temperature.
 
@@ -125,13 +119,12 @@ H2O_pressure_abs = sensor.getPressure(ADC_4096);
    * I am reading this as "higher ADC values means higher precision"
    */
 
-// Include a conversion from absolute pressure into relative
-// pressure (AKA, meters above sea level).
-
+// Include a conversion from absolute pressure into relative pressure (AKA, meters above sea level).
+// reference altitude here might be the barometric pressure from the Adafruit MPL115A2.
 H2O_pressure_relative = sealevel(H2O_pressure_abs, reference_altitude);
 
 // Report values
-Serial.print("pressure abs (mbar)= ");
+Serial.print("absolute water pressure = ");
 Serial.println(H2O_pressure_abs);
 
 /*
@@ -139,7 +132,7 @@ Serial.println(H2O_pressure_abs);
  * when desired, just copy the following quotation and paste into the script.
  * 
  * "
- * 
+
 Serial.print("Pressure relative (mbar)= ");
 Serial.println(H2O_pressure_relative);
 
@@ -217,8 +210,8 @@ String createDataRecord()
   String data = "";
   data += samplenum;      // Creates a string called "data" and enters in the sample number
   data += ",";            // Add a comma after sample number
-  data += (H2O_pressure_abs - (BaroPressure_mbar * 10));     // WaterLevel needs to be calculated. When ready, enter "data += WaterLevel" into the script.
-  samplenum++;            // increment the sample number (I think this means to add sample number to itself)
+  data += (H2O_pressure_abs - (BaroPressure_mbar * 10));     // This is recording the calculated water level to the file.
+  samplenum++;            // Increment the sample number
   return data;
 }
 
